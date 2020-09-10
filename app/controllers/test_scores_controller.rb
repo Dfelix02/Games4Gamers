46a396284433bba23f0533608ff190657d417cd2
Dfelix02 <david.felix02@hotmail.com>
2020-09-10 11:26:05 -0400

    some styling

--- app/controllers/test_scores_controller.rb
+++
@@ -1,6 +0,0 @@
-class TestScoresController < ApplicationController
-    def index
-        @testscores = @current_user.testscores
-    end
-
-end
--- app/controllers/tests_controller.rb
+++
@@ -1,9 +0,0 @@
-class TestsController < ApplicationController
-    def index
-        @tests = Text.all
-    end
-    
-    def show
-        @test = Text.find(params[:id])
-    end
-end
